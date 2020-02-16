122440a97e15902e96f0f273f7b3c0400e82bf3c
Toms Kedo <toms.kedo@gmail.com>
2020-02-16 11:13:12 +0000

    jquery final

--- w3_javascript/w3l04/script.js
+++ w3_javascript/w3l04/script.js
@@ -1,69 +1,79 @@
 $(function() {
-  $(".make-request").on("click", function() {
-    $.ajax({
-      method: "GET",
-      url: "https://reqres.in/api/users"
-    }).done(function(response) {
+  var $makeRequest = $('.make-request');
+  var $progress = $('.progress');
+  var page = 1;
+  var progressInterval;
+
+  $makeRequest.on('click', function() {
+      $.ajax({
+          method: "GET",
+          url: "https://reqres.in/api/users?page=" + page
+      }).done(handleResponse)
+  })
+
+  function handleResponse(response) {
       response.data.forEach(function(value, index) {
-        const img = '<img src="' + value.avatar + '">';
-        const row =
-          "\
+          const img = "<img src=\""+value.avatar+"\">"
+          const row = "\
           <tr>\
-            <td>" +
-          value.id +
-          '</td>\
-            <td class="cell-img">' +
-          img +
-          "</td>\
-            <td>" +
-          value.first_name +
-          "</td>\
-            <td>" +
-          value.last_name +
-          "</td>\
-            <td>" +
-          value.email +
-          "</td>\
+              "+ addCell(value.id, "cell-id") +"\
+              "+ addCell(img, "cell-img") +"\
+              "+ addCell(value.first_name) +"\
+              "+ addCell(value.last_name) +"\
+              "+ addCell(value.email) +"\
           </tr>";
 
-        $("#data-table")
-          .find("tbody")
-          .append(row);
-      });
-      $makeRequest.attr("disabled", true);
-    });
-  });
+          $('#data-table').find('tbody').append(row)
+      })
 
-  var makeRequest = function() {};
+      page ++;
 
-  $(".start-action").on("click", startProgress);
-  $(".stop-action").on("click", stopProgress);
-  $(".reset-action").on("click", resetProgress);
-
-  function startProgress() {
-    const cycleInterval = $("#cycle-interval").val();
-    let width = 0;
-
-    //   if (cycleInterval > 0) {
-    $(".progress").width(0);
-
-    progressInterval = setInterval(function() {
-      width++;
-      $(".progress").width(width + "%");
-
-      if (width >= 100) {
-        stopProgress();
+      if (!response.data.length) {
+          $makeRequest.attr('disabled', true);
       }
-    }, (cycleInterval * 1000) / 100);
-    //   }
   }
 
+  function addCell(val, className = "") {
+      return "<td class=\""+ className +"\">"+val+"</td>"
+  }
+
+  $('.start-action').on('click', startProgress)
+  $('.stop-action').on('click', stopProgress)
+  $('.reset-action').on('click', resetProgress)
+
+  function startProgress() {
+      const cycleInterval = $('#cycle-interval').val()
+      let width = 0;
+
+      if (cycleInterval > 0 ) {
+          $progress.width(0);
+
+          progressInterval = setInterval(function() {
+              width ++;
+              $progress.width(width + "%");
+
+              if (width >= 100) {
+                  stopProgress();
+              }
+          }, cycleInterval * 1000 / 100)
+      } else {
+          $('#cycle-interval').addClass('error')
+          $('.error-box').show()
+      }
+  }
+
+  $('#cycle-interval').on('focus', function() {
+      $('#cycle-interval').removeClass('error')
+      $('.error-box').hide()
+  })
+
   function stopProgress() {
-    clearInterval(progressInterval);
+      clearInterval(progressInterval);
   }
 
   function resetProgress() {
-    stopProgress();
-    $("#cycle-interval").val("");
+      stopProgress()
+      $('#cycle-interval').val("")
+      $progress.width(0);
   }
-});
+})
